d87bbae2cf395acd7a8a46b36caa705c3f1f5d8f
Fortune Domination <fortunedomination@gmail.com>
2025-09-01 11:38:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pin.it',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
