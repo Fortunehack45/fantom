b6d46d5ef88b43717c9276c103c1ca5295c0ba75
Fortune Domination <fortunedomination@gmail.com>
2025-09-05 14:02:22 +0000

    The features are still not working. Also change the icon.png to next.js

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,18 +4,14 @@ import { type SVGProps } from "react";
 export function GhostIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      role="img"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
       {...props}
     >
-      <path d="M20 4.7A2.2 2.2 0 0 0 17.8 4H6.2a2.2 2.2 0 0 0-2.2 2.2V14a2 2 0 0 0 2 2h2.2a2.5 2.5 0 0 1 2.3 1.25L12 21l1.3-1.75A2.5 2.5 0 0 1 15.6 18H18a2 2 0 0 0 2-2V6.7a2.2 2.2 0 0 0-2.2-2.2Z" />
-      <path d="M9 12h.01" />
-      <path d="M15 12h.01" />
+      <title>Next.js</title>
+      <path d="M12 0L2.363 5.432v13.136L12 24l9.637-5.432V5.432zM11.636 22.25v-8.84H2.727v-1.782l8.91-5.045v15.667zm.728 0V6.583l8.91 5.045v1.782h-8.91z" />
     </svg>
   );
 }
